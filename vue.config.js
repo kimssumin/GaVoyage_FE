a452d43721b9d82d4f6d38a1d0771b1f8293893c
KimSumin <ksumin0730@naver.com>
2023-05-04 09:22:24 +0900

    Chore : vue init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  outputDir: "../Gavoyage/src/main/resources/static",
+
+  devServer: {
+    proxy: {
+      "/": {
+        target: "http://localhost:8080",
+        chageOrigin: true,
+      },
+    },
+  },
+});
