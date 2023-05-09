f3c1134c23854c02eb517f9010d05629c7549569
KimSumin <ksumin0730@naver.com>
2023-05-09 12:08:50 +0900

    Feat : 화면 상단 이동 scroll 버튼

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,23 @@
   <div id="app">
     <Header></Header>
     <router-view />
+    <TopButton></TopButton>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
-import '@/assets/css/index.css';
+import "@/assets/css/index.css";
 
-import Header from './components/common/Header.vue';
-import Footer from './components/common/Footer.vue';
+import Header from "./components/common/Header.vue";
+import Footer from "./components/common/Footer.vue";
+import TopButton from "./components/button/TopButton.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Footer,
+    TopButton,
   },
 };
 </script>
--- src/components/button/TopButton.vue
+++ src/components/button/TopButton.vue
@@ -1,31 +1,44 @@
 <template>
-  <div>
-    <i class="fas fa-arrow-circle-up"></i>
-  </div>
+  <transition name="fade">
+    <div id="top" class="" v-show="scY > 300" @click="toTop">
+      <i class="fas fa-arrow-circle-up fa-2x"></i>
+    </div>
+  </transition>
 </template>
 <style>
 #top {
   position: fixed;
-  bottom: 90%;
-  right: 80%;
+  bottom: 3%;
+  right: 3%;
+}
+#top i {
+  color: rgba(161, 160, 160, 0.623);
 }
 </style>
 <script>
 export default {
+  data() {
+    return {
+      scTimer: 0,
+      scY: 0,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
   methods: {
-    top() {
-      let top = $("#top");
-      window.addEventListener("scroll", function () {
-        if (this.scrollY > 200) {
-          top.classList.add("on");
-        } else {
-          top.classList.remove("on");
-        }
-      });
-
-      top.addEventListener("click", function (e) {
-        e.preventDefault();
-        window.scrollTo({ top: 0, behavior: "smooth" });
+    handleScroll: function () {
+      if (this.scTimer) return;
+      this.scTimer = setTimeout(() => {
+        this.scY = window.scrollY;
+        clearTimeout(this.scTimer);
+        this.scTimer = 0;
+      }, 100);
+    },
+    toTop: function () {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
       });
     },
   },
