3570d9463eef895152e779c9a713575fe1a3856f
KimSumin <ksumin0730@naver.com>
2023-05-15 11:17:15 +0900

    Fix : Choose date necessary

--- src/assets/js/plan/planCreate.js
+++ src/assets/js/plan/planCreate.js
@@ -1,72 +1,72 @@
-import { selectFor } from '../map/selectBox';
-import { $, createElement } from '../util/elementTool';
+import { selectFor } from "../map/selectBox";
+import { $, createElement } from "../util/elementTool";
 
 export const planDate = () => {
-  const dailyPlan = document.querySelector('#dailyPlans');
-  const startDate = document.querySelector('#startDate').value;
-  const endDate = document.querySelector('#endDate').value;
+  const dailyPlan = $("#dailyPlans");
+  const startDate = $("#startDate").value;
+  const endDate = $("#endDate").value;
 
-  if (!startDate.includes('-') || !endDate.includes('-')) {
-    alert('날짜를 입력해주세요');
-  } else if (Number(startDate.split('-')[2]) > Number(endDate.split('-')[2])) {
-    alert('도착일자는 출발일자보다 빠를 수 없어요');
+  if (!startDate.includes("-") || !endDate.includes("-")) {
+    alert("날짜를 입력해주세요");
+  } else if (Number(startDate.split("-")[2]) > Number(endDate.split("-")[2])) {
+    alert("도착일자는 출발일자보다 빠를 수 없어요");
   } else {
     //여행 일자 계산
-    dailyPlan.innerHTML = '<h1>Plans</h1>';
+    dailyPlan.innerHTML = "<h1>Plans</h1>";
 
-    const totalDay = Number(endDate.split('-')[2]) - Number(startDate.split('-')[2]) + 1;
-    let optionsContainer = document.querySelector('.options-container');
-    optionsContainer.innerHTML = '';
+    const totalDay = Number(endDate.split("-")[2]) - Number(startDate.split("-")[2]) + 1;
+    let optionsContainer = $(".options-container");
+    optionsContainer.innerHTML = "";
 
     for (let i = 0; i < totalDay; i++) {
       if (i % 2 == 0) {
-        const newLine = document.createElement('div');
-        newLine.className = 'row';
-        newLine.id = 'plan' + i;
-        const newPlan = getPlanBox(i, Number(startDate.split('-')[2]));
+        const newLine = createElement("div");
+        newLine.className = "row";
+        newLine.id = "plan" + i;
+        const newPlan = getPlanBox(i, Number(startDate.split("-")[2]));
         newLine.appendChild(newPlan);
         dailyPlan.appendChild(newLine);
       } else {
-        const newLine = dailyPlan.querySelector('#plan' + (i - 1));
-        const newPlan = getPlanBox(i, Number(startDate.split('-')[2]));
+        const newLine = dailyPlan.querySelector("#plan" + (i - 1));
+        const newPlan = getPlanBox(i, Number(startDate.split("-")[2]));
         newLine.appendChild(newPlan);
       }
 
       //일자 선택 추가
-      let opt = createElement('div');
-      opt.className = 'option';
-      let inpt = createElement('input');
-      inpt.type = 'radio';
-      inpt.className = 'radio';
+      let opt = createElement("div");
+      opt.className = "option";
+      let inpt = createElement("input");
+      inpt.type = "radio";
+      inpt.className = "radio";
       inpt.name = i + 1;
       inpt.value = Number(i + 1);
-      let lnpt = createElement('label', 'Day ' + Number(i + 1));
+      let lnpt = createElement("label", "Day " + Number(i + 1));
       opt.appendChild(inpt);
       opt.appendChild(lnpt);
       optionsContainer.appendChild(opt);
 
-      document.querySelector('.selectDate').style.display = 'block';
+      $(".selectDate").style.display = "block";
     }
   }
 };
 
 const getPlanBox = (i, day) => {
-  const newPlan = document.createElement('article');
+  const newPlan = createElement("article");
   newPlan.className = `card fl-left planBox${i + 1}`;
-  const newDate = document.createElement('section');
-  newDate.className = 'date';
-  const dateDiv = document.createElement('time');
-  const dateSpan = document.createElement('span');
+  const newDate = createElement("section");
+  newDate.className = "date";
+  const dateDiv = createElement("time");
+  const dateSpan = createElement("span");
   dateSpan.innerHTML = day + i;
-  const dateSpan2 = document.createElement('span');
-  dateSpan2.innerHTML = 'Day ' + (i + 1);
+  const dateSpan2 = createElement("span");
+  dateSpan2.innerHTML = "Day " + (i + 1);
   dateDiv.appendChild(dateSpan);
   dateDiv.appendChild(dateSpan2);
   newDate.appendChild(dateDiv);
   newPlan.appendChild(newDate);
 
-  const newContent = document.createElement('section');
-  newContent.className = 'card-cont';
+  const newContent = createElement("section");
+  newContent.className = "card-cont";
   newPlan.appendChild(newContent);
   return newPlan;
 };
@@ -75,10 +75,14 @@ const day = {};
 export function addPlan() {
   console.log(day);
   // const createBtn = document.querySelector("#addPlanBtn");
-  const nowDate = $('#selectedContent').value;
-  const dataTitle = document.querySelector('#data-title').innerHTML;
-  const dataId = document.querySelector('#data-id').innerHTML;
-
+  const nowDate = $("#selectedContent").value;
+  const dataTitle = $("#data-title").innerHTML;
+  const dataId = $("#data-id").innerHTML;
+  console.log(nowDate, typeof nowDate);
+  if (nowDate == undefined) {
+    alert("일정 날짜를 선택해주세요 !");
+    return;
+  }
   console.log(dataTitle, dataId);
   if (nowDate in day) {
     let check = false;
@@ -99,30 +103,30 @@ export function addPlan() {
 
 function deletePlan(content_id, now) {
   day[now] = day[now].filter((ele) => ele.id != content_id);
-  alert('삭제되었습니다');
-  console.log('here', day[now]);
+  alert("삭제되었습니다");
+  console.log("here", day[now]);
   showDailyPlan(now);
 }
 
 function showDailyPlan(nowDate) {
-  const planBox = document.querySelector('.planBox' + nowDate);
-  const eachPlan = planBox.querySelector('.card-cont');
-  eachPlan.innerHTML = '';
+  const planBox = $(".planBox" + nowDate);
+  const eachPlan = planBox.querySelector(".card-cont");
+  eachPlan.innerHTML = "";
 
   day[nowDate].forEach((attr, idx) => {
-    let div = document.createElement('div');
-    div.className = 'even-info';
+    let div = createElement("div");
+    div.className = "even-info";
     div.innerHTML = `<i class="fa fa-map-marker me-2"></i>`;
 
-    let ptext = document.createElement('p');
+    let ptext = createElement("p");
     ptext.innerHTML = attr.title;
-    let dbtn = document.createElement('button');
-    dbtn.className = 'deleteBtn' + idx;
-    dbtn.id = 'deleteBtn';
-    dbtn.innerHTML = 'DELETE';
+    let dbtn = createElement("button");
+    dbtn.className = "deleteBtn" + idx;
+    dbtn.id = "deleteBtn";
+    dbtn.innerHTML = "DELETE";
     div.appendChild(ptext);
     div.appendChild(dbtn);
-    dbtn.addEventListener('click', () => {
+    dbtn.addEventListener("click", () => {
       deletePlan(attr.id, nowDate);
     });
     eachPlan.appendChild(div);
@@ -131,14 +135,14 @@ function showDailyPlan(nowDate) {
 
 // const resultBtn = document.querySelector("#resultBtn");
 export function submitResult() {
-  const result = { title: '', startDate: '', endDate: '', plan: {} };
+  const result = { title: "", startDate: "", endDate: "", plan: {} };
   result.plan = day;
-  result.title = document.querySelector('#title').value;
-  result.startDate = document.querySelector('#startDate').value;
-  result.endDate = document.querySelector('#endDate').value;
+  result.title = $("#title").value;
+  result.startDate = $("#startDate").value;
+  result.endDate = $("#endDate").value;
   console.log(result);
 }
 
 export function selectDate() {
-  selectFor('.selectDate');
+  selectFor(".selectDate");
 }
--- src/components/plan/PlanCreate.vue
+++ src/components/plan/PlanCreate.vue
@@ -29,7 +29,9 @@
                     <label for="">Select Date</label>
                   </div>
                 </div>
-                <div class="selected" id="selectedContent" v-on:click="selectDate">Select Date</div>
+                <div class="selected" id="selectedContent" v-on:click="selectDate" value="0">
+                  Select Date
+                </div>
               </div>
               <SelectBox></SelectBox>
             </div>
@@ -72,14 +74,14 @@
   <button class="btn-get-started" type="button" @click="addPlan">등록</button>
 </div> -->
 <script>
-import { addPlan, planDate, selectDate, submitResult } from '@/assets/js/plan/planCreate.js';
-import TopButton from '@/components/button/TopButton.vue';
-import SelectBox from '../button/SelectBox.vue';
-import getAttractionInfo from '../map/getAttractionInfo.vue';
-import getMap from '../map/getMap.vue';
+import { addPlan, planDate, selectDate, submitResult } from "@/assets/js/plan/planCreate.js";
+import TopButton from "@/components/button/TopButton.vue";
+import SelectBox from "../button/SelectBox.vue";
+import getAttractionInfo from "../map/getAttractionInfo.vue";
+import getMap from "../map/getMap.vue";
 
 export default {
-  name: 'PlanCreate',
+  name: "PlanCreate",
   props: {},
   components: {
     TopButton,
@@ -223,7 +225,7 @@ h1 {
 }
 
 .selected::after {
-  content: '';
+  content: "";
   /* background: url("@/assets/img/arrow-down.svg"); */
   background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"/></svg>');
   background-repeat: no-repeat;
@@ -319,7 +321,7 @@ h1 {
 
 .date:before,
 .date:after {
-  content: '';
+  content: "";
   display: block;
   width: 30px;
   height: 30px;
