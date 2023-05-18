391e20bf3667b804011be446093a642b1ced6378
KimSumin <ksumin0730@naver.com>
2023-05-18 14:09:35 +0900

    Chore : Fixed router mode

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,24 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require("@vue/cli-service");
 // const baseURL = require('./src/assets/js/util/url');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  outputDir: '../Gavoyage/src/main/resources/static',
+  outputDir: "../Gavoyage/src/main/resources/static",
 
   devServer: {
     proxy: {
-      '/': {
-        target: 'http://localhost:8080',
+      "/": {
+        target: "http://localhost:8080",
         chageOrigin: true,
         ws: false,
       },
+      // "/plan*": {
+      //   target: "http://localhost:8080/plan",
+      //   changeOrigin: true,
+      // },
     },
+    historyApiFallback: true,
   },
 });
 //"http://70.12.50.218:8080",
