bd451f6cb5484423c2b4d17b8ccf03774e200d7d
Sumin Kim <ksumin0730@hufs.ac.kr>
2023-05-24 23:51:49 +0900

    Feat : Create Review thumbnail

--- src/components/board/ReviewBoard.vue
+++ src/components/board/ReviewBoard.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="box">
     <img
+      v-if="!isImg"
       class="img"
       src="https://img.freepik.com/free-psd/travel-background-composition-with-backpack_23-2149603154.jpg?w=1380&t=st=1684904625~exp=1684905225~hmac=d5a23928d33003ee6f767f2056d9cd7246fb29951045388d35f0212c3ce100ca"
       alt=""
     />
+    <img v-else-if="isImg" class="img" :src="imgList[0]" alt="" />
     <!--"../assets/img/korea-geab21995b_1920.jpg"-->
     <div class="heading">
       <h6><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;GaVoyage</h6>
@@ -47,15 +49,37 @@ export default {
       let dataObj = { content_id: unrecommendAttr.content_id, value: unrecommendAttr.isScrab };
       this.$store.dispatch('buttonStore/updateScrap', dataObj, { root: true });
     });
+
+    this.imgList = this.extractImgSrcFromHtmlString(this.review.contents);
+    console.log('이미지 추출', this.imgList);
+    if (this.imgList.length !== 0) {
+      this.isImg = true;
+    }
   },
 
   data() {
     return {
       reviewLike: { id: this.review.reviewIdx, isLiked: 0 },
+      isImg: false,
+      imgList: [],
     };
   },
   components: {
     Like,
+  },
+
+  methods: {
+    extractImgSrcFromHtmlString(htmlString) {
+      const imgRegex = /<img.*?src=["'](.*?)["']/g;
+      const matches = htmlString.matchAll(imgRegex);
+      const srcList = [];
+
+      for (const match of matches) {
+        srcList.push(match[1]);
+      }
+
+      return srcList;
+    },
   },
 };
 </script>
